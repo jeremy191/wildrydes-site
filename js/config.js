b6837843136f28d3cc0f6c93a3306a63b8490540
Jeremy Perez <jeremy.perez2@upr.edu>
2020-09-21 20:35:16 -0400

    updating config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'Id us-east-1_X0luK7Gl4', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '32hsbd2a8idrjc3kke3rg7t43h', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
